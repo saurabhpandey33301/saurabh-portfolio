850a088efcf3f96345bcf3e58a596f58af869f13
saurabhpandey33301 <124060265+saurabhpandey33301@users.noreply.github.com>
2025-06-09 18:43:11 +0530

    updated new projects in my projects section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,5 +1,3 @@
-
-
 import { ReactLenis } from "lenis/react";
 import { Meteors } from "@/components/ui/meteors";
 import Hero from "@/components/Hero";
@@ -7,61 +5,54 @@ import Projects from "@/components/projects";
 import Skills from "@/components/Skills";
 import About from "@/components/About";
 
-
 import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div className="max-w-full min-h-screen">
       <ReactLenis root>
-       {/* Meteors Background */}
-<Meteors number={40} />
+        {/* Meteors Background */}
+        <Meteors number={40} />
 
-{/* Hero Section */}
-<section 
-  id="hero" 
-  className="hero w-full h-screen flex justify-start items-center overflow-hidden"
->
-  <Hero />
-</section>
+        {/* Hero Section */}
+        <section
+          id="hero"
+          className="hero w-full h-screen flex justify-start items-center overflow-hidden"
+        >
+          <Hero />
+        </section>
 
-{/* Background Image Section */}
-<section  
-  className="followUp max-w-full h-[400px] flex justify-center items-center bg-no-repeat bg-center bg-contain overflow-hidden"
->
-</section>
+        {/* Background Image Section */}
+        <section className="followUp max-w-full h-[400px] flex justify-center items-center bg-no-repeat bg-center bg-contain overflow-hidden"></section>
 
-{/* Projects Section */}
-<section 
-  id="projects" 
-  className="Projects w-full min-h-screen flex flex-col items-center justify-center px-4 md:px-16"
->
-  <Projects />
-</section>
+        {/* Projects Section */}
+        <section
+          id="projects"
+          className="Projects w-full min-h-screen flex flex-col items-center justify-center px-4 md:px-16"
+        >
+          <Projects />
+        </section>
 
-{/* Tech Stack Section */}
-<section 
-  id="skills" 
-  className="Projects w-full min-h-screen flex flex-col items-center justify-center px-4 md:px-16"
->
-  <Skills />
-</section>
+        {/* Tech Stack Section */}
+        <section
+          id="skills"
+          className="Projects w-full min-h-screen flex flex-col items-center justify-center px-4 md:px-16"
+        >
+          <Skills />
+        </section>
 
-{/* About Section */}
-<section 
-  id="about"  
-  className="about w-full min-h-screen flex flex-col md:flex-row justify-center items-center px-4 md:px-16"
->
-  <About />
-</section>
+        {/* About Section */}
+        <section
+          id="about"
+          className="about w-full min-h-screen flex flex-col md:flex-row justify-center items-center px-4 md:px-16"
+        >
+          <About />
+        </section>
 
-{/* Footer Section */}
-<footer 
-  className="footer w-full border-t flex justify-center items-center h-auto md:h-[300px] px-4 md:px-16"
-> 
-  <Footer /> 
-</footer>
-
+        {/* Footer Section */}
+        <footer className="footer w-full border-t flex justify-center items-center h-auto md:h-[300px] px-4 md:px-16">
+          <Footer />
+        </footer>
       </ReactLenis>
     </div>
   );
--- portfolio/components/grid.tsx
+++ portfolio/components/grid.tsx
@@ -1,14 +1,11 @@
-
-import img from "../asset/blog.jpg"
-import imgLeet from "@/asset/leetmetric.png"
-import imgpay from "@/asset/qikpay.png"
+import img from "../asset/blog.jpg";
+import imgLeet from "@/asset/leetmetric.png";
+import imgpay from "@/asset/qikpay.png";
 import React from "react";
-import imgNews from "@/asset/newsletter.png"
 import { BentoGrid, BentoGridItem } from "../components/ui/bento-grid";
-import imgXyz from "@/asset/xyz.png"
+import imgXyz from "@/asset/xyz.png";
 import Link from "next/link";
 import { Globe } from "lucide-react";
-
 
 export function BentoGridDemo() {
   return (
@@ -21,79 +18,119 @@ export function BentoGridDemo() {
           header={item.header}
           icon={item.icon}
           link={item.link}
-          className={i === 3 || i === 6 ? "md:col-span-2 bg-[#efe4e42a]" : "bg-[#efe4e42a]"}
+          className={
+            i === 3 || i === 6
+              ? "md:col-span-2 bg-[#efe4e42a]"
+              : "bg-[#efe4e42a]"
+          }
         />
       ))}
     </BentoGrid>
   );
 }
 const Skeleton = () => (
-//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  <img src={img.src} alt="xyz" className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl ps-1 pe-1" />
+  //   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+  <img
+    src={img.src}
+    alt="xyz"
+    className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl ps-1 pe-1"
+  />
 );
 const SkeletonLeet = () => (
-//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  <img src={imgLeet.src} alt="xyz" className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl " />
+  //   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+  <img
+    src={imgLeet.src}
+    alt="xyz"
+    className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl "
+  />
 );
 const SkeletonPay = () => (
-//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  <img src={imgpay.src} alt="xyz" className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl " />
+  //   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+  <img
+    src={imgpay.src}
+    alt="xyz"
+    className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl "
+  />
 );
 const SkeletonNews = () => (
-//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  <img src={imgNews.src} alt="xyz" className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl " />
+  //   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+  <img
+    src={"weather.png"}
+    alt="xyz"
+    className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl "
+  />
 );
 const SkeletonXyz = () => (
-//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-  <img src={imgXyz.src} alt="xyz" className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl " />
+  //   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+  <img
+    src={"snip-ai.png"}
+    alt="xyz"
+    className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl "
+  />
 );
 const items = [
   {
     title: "WordfloW",
-    description: "An immersive blog web app crafted for passionate writers and avid readers, fostering a seamless storytelling experience.",
+    description:
+      "An immersive blog web app crafted for passionate writers and avid readers, fostering a seamless storytelling experience.",
     header: <Skeleton />,
-    icon: <Link href={"https://wordflow-woad.vercel.app/"}  target="_blank"><Globe className="w-6 h-6 text-white hover:text-lime-400"/></Link>,
-    link: "https://wordflow-woad.vercel.app/"
-  },
-  {
-    title: "New Project",
-    description: "Work in progress on a new project that will be uploaded soon....",
-    header: <SkeletonXyz />,
-    icon: <Link href={""} target="_blank"><Globe className="w-6 h-6 text-white  hover:text-lime-400"/></Link>,
-     link : ""
-  },
-  {
-    title: "LEETMETRIC",
-    description: "Unlock your LeetCode stats effortlessly—just enter your username and dive into your coding journey!",
-    header: <SkeletonLeet/>,
-    icon: <Link href={"https://saurabhpandey33301.github.io/LEETMETRIC/"} target="_blank"><Globe className="w-6 h-6 text-white  hover:text-lime-400"/></Link>,
-     link : "https://saurabhpandey33301.github.io/LEETMETRIC/"
+    icon: (
+      <Link href={"https://wordflow-woad.vercel.app/"} target="_blank">
+        <Globe className="w-6 h-6 text-white hover:text-lime-400" />
+      </Link>
+    ),
+    link: "https://wordflow-woad.vercel.app/",
   },
   {
     title: "QikPay",
     description:
-      "A cutting-edge payment app designed for a seamless and hassle-free transaction experience. Users can effortlessly transfer money to others and make quick bank transactions with ease.",
+      "A cutting-edge payment app designed to deliver a seamless, secure, and hassle-free transaction experience for users across all platforms.",
     header: <SkeletonPay />,
-    icon: <Link href={"https://qik-pay.vercel.app/"} target="_blank"><Globe className="w-6 h-6 text-white hover:text-lime-400"/></Link>,
-     link : "https://qik-pay.vercel.app/"
+    icon: (
+      <Link href={"https://qik-pay.vercel.app/"} target="_blank">
+        <Globe className="w-6 h-6 text-white hover:text-lime-400" />
+      </Link>
+    ),
+    link: "https://qik-pay.vercel.app/",
   },
   {
-    title: "Newsletter box",
-    description: "A sleek and intuitive newsletter box application designed to captivate audiences and streamline content delivery with ease." ,
-    header: <SkeletonNews />,
-    icon: <Link href={"https://saurabhnewsletter.onrender.com/"} target="_blank"><Globe className="w-6 h-6 text-white  hover:text-lime-400" /></Link>,
-     link : "https://saurabhnewsletter.onrender.com/"
+    title: "LEETMETRIC",
+    description:
+      "Unlock your LeetCode stats effortlessly—just enter your username and dive into your coding journey!",
+    header: <SkeletonLeet />,
+    icon: (
+      <Link
+        href={"https://saurabhpandey33301.github.io/LEETMETRIC/"}
+        target="_blank"
+      >
+        <Globe className="w-6 h-6 text-white  hover:text-lime-400" />
+      </Link>
+    ),
+    link: "https://saurabhpandey33301.github.io/LEETMETRIC/",
   },
-//   {
-//     title: "The Joy of Creation",
-//     description: "Experience the thrill of bringing ideas to life.",
-//     header: <Skeleton />,
-//     icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-//   },
-//   {
-//     title: "The Spirit of Adventure",
-//     description: "Embark on exciting journeys and thrilling discoveries.",
-//     header: <Skeleton />,
-//     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-//   },
+  {
+    title: "Snip-Ai",
+    description:
+      "Generate stunning short videos with AI-written scripts, dynamic captions, voiceovers, and images — all in one click. Perfect for creating reels, shorts, and social media content at scale.",
+    header: <SkeletonXyz />,
+    icon: (
+      <Link href={"https://snip-ai-seven.vercel.app/"} target="_blank">
+        <Globe className="w-6 h-6 text-white hover:text-lime-400" />
+      </Link>
+    ),
+    link: "https://snip-ai-seven.vercel.app/",
+  },
+  {
+    title: "Weather-based-suggestor",
+    description:
+      "A Weather-Based Activity Suggestor recommends the best activities to do based on current or forecasted weather conditions. It helps users plan their day by offering personalized ideas like hiking on sunny days or movie marathons when it rains.",
+    header: <SkeletonNews />,
+    icon: (
+      <Link href={"https://weather-based-suggestor.vercel.app/"} target="_blank">
+        <Globe className="w-6 h-6 text-white  hover:text-lime-400" />
+      </Link>
+    ),
+    link: "https://weather-based-suggestor.vercel.app/",
+  },
+  
 ];
